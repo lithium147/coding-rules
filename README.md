9641c5248431b6c9cafb2c0d52b1906413e9ce24
twalters <timlwalters@yahoo.co.uk>
2026-03-01 23:11:23 +0000

    more StreamSugar

---
+++ README.md
@@ -0,0 +1,5 @@
+
+# TODO create syntactic sugar library
+
+
+
--- src/main/java/com/solubris/StreamSugar.java
+++ src/main/java/com/solubris/StreamSugar.java
@@ -2,8 +2,19 @@ package com.solubris;
 
 import java.util.stream.Stream;
 
+import static java.util.function.Function.identity;
+
 public class StreamSugar {
     public static <T> Stream<? extends T> prepend(T t, Stream<T> tStream) {
         return Stream.concat(Stream.of(t), tStream);
+    }
+
+    public static <T> Stream<? extends T> append(Stream<T> tStream, T t) {
+        return Stream.concat(tStream, Stream.of(t));
+    }
+
+    @SafeVarargs
+    public static <T> Stream<? extends T> concat(Stream<T>... tStreams) {
+        return Stream.of(tStreams).flatMap(identity());
     }
 }
