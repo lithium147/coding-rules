cf3d8162542e800dbef562cd5601a164150601f8
twalters <timlwalters@yahoo.co.uk>
2026-03-06 01:33:57 +0000

    clean up the test built by devin

--- src/test/java/com/solubris/enforcer/UnusedPropertyRuleTest.java
+++ src/test/java/com/solubris/enforcer/UnusedPropertyRuleTest.java
@@ -5,6 +5,7 @@ import org.apache.maven.enforcer.rule.api.EnforcerRuleException;
 import org.apache.maven.model.Build;
 import org.apache.maven.model.DependencyManagement;
 import org.apache.maven.model.Model;
+import org.apache.maven.model.Plugin;
 import org.apache.maven.model.PluginManagement;
 import org.apache.maven.model.Reporting;
 import org.junit.jupiter.api.Test;
@@ -20,31 +21,28 @@ import static org.assertj.core.api.Assertions.assertThatNoException;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.mock;
 
-/**
- * Test cases for UnusedPropertyRule enforcer rule.
- */
 class UnusedPropertyRuleTest {
+    private final Model originalModel = new Model();
+    private final Model effectiveModel = new Model();
+    private final UnusedPropertyRule rule;
+
+    UnusedPropertyRuleTest() {
+        UnusedPropertyRule rule = new UnusedPropertyRule(originalModel, effectiveModel);
+        rule.setLog(mock(EnforcerLogger.class));
+        this.rule = rule;
+    }
 
     @Test
     void noPropertiesPasses() {
-        Model originalModel = new Model();
-        Model effectiveModel = new Model();
-
-        UnusedPropertyRule rule = createRule(originalModel, effectiveModel);
-
         assertThatNoException().isThrownBy(rule::execute);
     }
 
     @Test
     void versionPropertyUsedByDirectDependencyPasses() {
-        Model originalModel = new Model();
         originalModel.addProperty("junit.version", "5.9.3");
         originalModel.addDependency(dependencyOf("org.junit", "junit", "${junit.version}"));
 
-        Model effectiveModel = new Model();
         effectiveModel.addDependency(dependencyOf("org.junit", "junit", "5.9.3"));
-
-        UnusedPropertyRule rule = createRule(originalModel, effectiveModel);
 
         Stream<String> violations = rule.scanProperties();
 
@@ -53,18 +51,14 @@ class UnusedPropertyRuleTest {
 
     @Test
     void versionPropertyUsedByManagedDependencyPasses() {
-        Model originalModel = new Model();
         originalModel.addProperty("junit.version", "5.9.3");
         DependencyManagement originalDepMgmt = new DependencyManagement();
         originalDepMgmt.addDependency(dependencyOf("org.junit", "junit", "${junit.version}"));
         originalModel.setDependencyManagement(originalDepMgmt);
 
-        Model effectiveModel = new Model();
         DependencyManagement effectiveDepMgmt = new DependencyManagement();
         effectiveDepMgmt.addDependency(dependencyOf("org.junit", "junit", "5.9.3"));
         effectiveModel.setDependencyManagement(effectiveDepMgmt);
-
-        UnusedPropertyRule rule = createRule(originalModel, effectiveModel);
 
         Stream<String> violations = rule.scanProperties();
 
@@ -73,18 +67,14 @@ class UnusedPropertyRuleTest {
 
     @Test
     void versionPropertyUsedByPluginPasses() {
-        Model originalModel = new Model();
         originalModel.addProperty("compiler.version", "3.13.0");
         Build originalBuild = new Build();
         originalBuild.addPlugin(pluginOf("org.apache.maven.plugins", "maven-compiler-plugin", "${compiler.version}"));
         originalModel.setBuild(originalBuild);
 
-        Model effectiveModel = new Model();
         Build effectiveBuild = new Build();
         effectiveBuild.addPlugin(pluginOf("org.apache.maven.plugins", "maven-compiler-plugin", "3.13.0"));
         effectiveModel.setBuild(effectiveBuild);
-
-        UnusedPropertyRule rule = createRule(originalModel, effectiveModel);
 
         Stream<String> violations = rule.scanProperties();
 
@@ -93,7 +83,6 @@ class UnusedPropertyRuleTest {
 
     @Test
     void versionPropertyUsedByManagedPluginPasses() {
-        Model originalModel = new Model();
         originalModel.addProperty("compiler.version", "3.13.0");
         Build originalBuild = new Build();
         PluginManagement originalPluginMgmt = new PluginManagement();
@@ -101,14 +90,11 @@ class UnusedPropertyRuleTest {
         originalBuild.setPluginManagement(originalPluginMgmt);
         originalModel.setBuild(originalBuild);
 
-        Model effectiveModel = new Model();
         Build effectiveBuild = new Build();
         PluginManagement effectivePluginMgmt = new PluginManagement();
         effectivePluginMgmt.addPlugin(pluginOf("org.apache.maven.plugins", "maven-compiler-plugin", "3.13.0"));
         effectiveBuild.setPluginManagement(effectivePluginMgmt);
         effectiveModel.setBuild(effectiveBuild);
-
-        UnusedPropertyRule rule = createRule(originalModel, effectiveModel);
 
         Stream<String> violations = rule.scanProperties();
 
@@ -117,18 +103,14 @@ class UnusedPropertyRuleTest {
 
     @Test
     void versionPropertyUsedByExtensionPasses() {
-        Model originalModel = new Model();
         originalModel.addProperty("ext.version", "1.0.0");
         Build originalBuild = new Build();
         originalBuild.addExtension(extensionOf("org.example", "my-extension", "${ext.version}"));
         originalModel.setBuild(originalBuild);
 
-        Model effectiveModel = new Model();
         Build effectiveBuild = new Build();
         effectiveBuild.addExtension(extensionOf("org.example", "my-extension", "1.0.0"));
         effectiveModel.setBuild(effectiveBuild);
-
-        UnusedPropertyRule rule = createRule(originalModel, effectiveModel);
 
         Stream<String> violations = rule.scanProperties();
 
@@ -137,18 +119,14 @@ class UnusedPropertyRuleTest {
 
     @Test
     void versionPropertyUsedByReportPluginPasses() {
-        Model originalModel = new Model();
         originalModel.addProperty("site.version", "4.0.0");
         Reporting originalReporting = new Reporting();
         originalReporting.addPlugin(reportPluginOf("org.apache.maven.plugins", "maven-site-plugin", "${site.version}"));
         originalModel.setReporting(originalReporting);
 
-        Model effectiveModel = new Model();
         Reporting effectiveReporting = new Reporting();
         effectiveReporting.addPlugin(reportPluginOf("org.apache.maven.plugins", "maven-site-plugin", "4.0.0"));
         effectiveModel.setReporting(effectiveReporting);
-
-        UnusedPropertyRule rule = createRule(originalModel, effectiveModel);
 
         Stream<String> violations = rule.scanProperties();
 
@@ -157,12 +135,7 @@ class UnusedPropertyRuleTest {
 
     @Test
     void unusedVersionPropertyFails() {
-        Model originalModel = new Model();
         originalModel.addProperty("old-lib.version", "2.0.0");
-
-        Model effectiveModel = new Model();
-
-        UnusedPropertyRule rule = createRule(originalModel, effectiveModel);
 
         Stream<String> violations = rule.scanProperties();
 
@@ -177,12 +150,7 @@ class UnusedPropertyRuleTest {
 
     @Test
     void nonVersionPropertyIsIgnored() {
-        Model originalModel = new Model();
         originalModel.addProperty("project.build.sourceEncoding", "UTF-8");
-
-        Model effectiveModel = new Model();
-
-        UnusedPropertyRule rule = createRule(originalModel, effectiveModel);
 
         Stream<String> violations = rule.scanProperties();
 
@@ -191,15 +159,11 @@ class UnusedPropertyRuleTest {
 
     @Test
     void multiplePropertiesMixedUsage() {
-        Model originalModel = new Model();
         originalModel.addProperty("junit.version", "5.9.3");
         originalModel.addProperty("old-lib.version", "2.0.0");
         originalModel.addDependency(dependencyOf("org.junit", "junit", "${junit.version}"));
 
-        Model effectiveModel = new Model();
         effectiveModel.addDependency(dependencyOf("org.junit", "junit", "5.9.3"));
-
-        UnusedPropertyRule rule = createRule(originalModel, effectiveModel);
 
         Stream<String> violations = rule.scanProperties();
 
@@ -210,13 +174,8 @@ class UnusedPropertyRuleTest {
 
     @Test
     void multipleUnusedPropertiesReported() {
-        Model originalModel = new Model();
         originalModel.addProperty("lib-a.version", "1.0.0");
         originalModel.addProperty("lib-b.version", "2.0.0");
-
-        Model effectiveModel = new Model();
-
-        UnusedPropertyRule rule = createRule(originalModel, effectiveModel);
 
         Stream<String> violations = rule.scanProperties();
 
@@ -228,60 +187,39 @@ class UnusedPropertyRuleTest {
 
     @Test
     void executeThrowsOnUnusedProperty() {
-        Model originalModel = new Model();
         originalModel.addProperty("orphaned.version", "3.0.0");
 
-        Model effectiveModel = new Model();
-
-        UnusedPropertyRule rule = createRule(originalModel, effectiveModel);
-
         assertThatThrownBy(rule::execute)
-                .isInstanceOf(EnforcerRuleException.class)
-                .hasMessageContaining("orphaned.version")
-                .hasMessageContaining("3.0.0");
+                .isInstanceOf(EnforcerRuleException.class);
     }
 
     @Test
     void executePassesWhenAllPropertiesUsed() {
-        Model originalModel = new Model();
         originalModel.addProperty("junit.version", "5.9.3");
         originalModel.addDependency(dependencyOf("org.junit", "junit", "${junit.version}"));
 
-        Model effectiveModel = new Model();
         effectiveModel.addDependency(dependencyOf("org.junit", "junit", "5.9.3"));
-
-        UnusedPropertyRule rule = createRule(originalModel, effectiveModel);
 
         assertThatNoException().isThrownBy(rule::execute);
     }
 
     @Test
     void versionPropertyUsedByPluginDependencyPasses() {
-        Model originalModel = new Model();
         originalModel.addProperty("api.version", "3.0.0");
         Build originalBuild = new Build();
-        var plugin = pluginOf("org.apache.maven.plugins", "maven-compiler-plugin", "3.13.0");
+        Plugin plugin = pluginOf("org.apache.maven.plugins", "maven-compiler-plugin", "3.13.0");
         plugin.addDependency(dependencyOf("org.apache.maven", "maven-plugin-api", "${api.version}"));
         originalBuild.addPlugin(plugin);
         originalModel.setBuild(originalBuild);
 
-        Model effectiveModel = new Model();
         Build effectiveBuild = new Build();
-        var effectivePlugin = pluginOf("org.apache.maven.plugins", "maven-compiler-plugin", "3.13.0");
+        Plugin effectivePlugin = pluginOf("org.apache.maven.plugins", "maven-compiler-plugin", "3.13.0");
         effectivePlugin.addDependency(dependencyOf("org.apache.maven", "maven-plugin-api", "3.0.0"));
         effectiveBuild.addPlugin(effectivePlugin);
         effectiveModel.setBuild(effectiveBuild);
 
-        UnusedPropertyRule rule = createRule(originalModel, effectiveModel);
-
         Stream<String> violations = rule.scanProperties();
 
         assertThat(violations).isEmpty();
-    }
-
-    private static UnusedPropertyRule createRule(Model originalModel, Model effectiveModel) {
-        UnusedPropertyRule rule = new UnusedPropertyRule(originalModel, effectiveModel);
-        rule.setLog(mock(EnforcerLogger.class));
-        return rule;
     }
 }
