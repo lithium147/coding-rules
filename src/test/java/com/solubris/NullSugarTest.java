577818de136198c2fb3acdd00d58f3ec504a6add
twalters <timlwalters@yahoo.co.uk>
2026-03-08 23:03:24 +0000

    null sugar tests

---
+++ src/test/java/com/solubris/NullSugarTest.java
@@ -0,0 +1,32 @@
+package com.solubris;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class NullSugarTest {
+    @Nested
+    class Coalesce {
+        @Test
+        void canReturnFirstValue() {
+            String result = NullSugar.coalesce("first", "second", "third");
+
+            assertThat(result).isEqualTo("first");
+        }
+
+        @Test
+        void canReturnSecondValue() {
+            String result = NullSugar.coalesce(null, "second", "third");
+
+            assertThat(result).isEqualTo("second");
+        }
+
+        @Test
+        void coalesce() {
+            String result = NullSugar.coalesce(null, null);
+
+            assertThat(result).isNull();
+        }
+    }
+}
